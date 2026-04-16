5065e7d5876e9d84a9220f7aa7fb4b38fe3b9908
Gustavo de los Santos <gustleo.dev@gmail.com>
2026-04-16 22:32:52 +0200

    feat: added basic `GameHandler` methods implementations

--- services/rest/api/handlers/games.go
+++ services/rest/api/handlers/games.go
@@ -1,9 +1,13 @@
 package handlers
 
 import (
+	"encoding/json"
 	"net/http"
+	"strconv"
 
 	"github.com/condemo/game-organizer/services/common/service"
+	"github.com/condemo/game-organizer/services/common/types"
+	"github.com/condemo/game-organizer/services/rest/api/errs"
 	"github.com/go-chi/chi"
 )
 
@@ -23,42 +27,107 @@ func (h *GameHandler) RegisterRoutes() http.Handler {
 	r.Get("/search", MakeHandler(h.search))
 	r.Put("/", MakeHandler(h.createGame))
 	r.Put("/", MakeHandler(h.updateGame))
-	r.Delete("/", MakeHandler(h.deleteGame))
+	r.Delete("/{id}", MakeHandler(h.deleteGame))
 
 	return r
 }
 
-// TODO:
 func (h *GameHandler) getFetchGame(w http.ResponseWriter, r *http.Request) error {
+	idStr := chi.URLParam(r, "id")
+	id, err := strconv.ParseInt(idStr, 10, 64)
+	if err != nil {
+		return errs.NewApiError(err, http.StatusBadRequest, "game id must be a number")
+	}
+
+	game, err := h.service.GetFetchGame(id)
+	if err != nil {
+		return err
+	}
+
+	JsonResponse(w, http.StatusOK, game)
+
 	return nil
 }
 
-// TODO:
 func (h *GameHandler) getOneGame(w http.ResponseWriter, r *http.Request) error {
+	idStr := chi.URLParam(r, "id")
+	id, err := strconv.ParseInt(idStr, 10, 64)
+	if err != nil {
+		return errs.NewApiError(err, http.StatusBadRequest, "game id must be a number")
+	}
+
+	game, err := h.service.GetOneGame(id)
+	if err != nil {
+		return err
+	}
+
+	JsonResponse(w, http.StatusOK, game)
+
 	return nil
 }
 
-// TODO:
 func (h *GameHandler) getGames(w http.ResponseWriter, r *http.Request) error {
+	games, err := h.service.GetGames()
+	if err != nil {
+		return err
+	}
+
+	JsonResponse(w, http.StatusOK, games)
 	return nil
 }
 
-// TODO:
 func (h *GameHandler) search(w http.ResponseWriter, r *http.Request) error {
+	res, err := h.service.Search("")
+	if err != nil {
+		return err
+	}
+
+	JsonResponse(w, http.StatusOK, res)
 	return nil
 }
 
-// TODO:
 func (h *GameHandler) createGame(w http.ResponseWriter, r *http.Request) error {
+	var game types.Game
+
+	if err := json.NewDecoder(r.Body).Decode(&game); err != nil {
+		return errs.NewApiError(err, http.StatusBadRequest, "invalid json format")
+	}
+
+	if err := h.service.CreateGame(&game); err != nil {
+		return err
+	}
+
+	JsonResponse(w, http.StatusCreated, game)
 	return nil
 }
 
-// TODO:
 func (h *GameHandler) updateGame(w http.ResponseWriter, r *http.Request) error {
+	var game types.Game
+
+	if err := json.NewDecoder(r.Body).Decode(&game); err != nil {
+		return errs.NewApiError(err, http.StatusBadRequest, "invalid json format")
+	}
+
+	if err := h.service.UpdateGame(&game); err != nil {
+		return err
+	}
+
+	JsonResponse(w, http.StatusCreated, game)
 	return nil
 }
 
-// TODO:
 func (h *GameHandler) deleteGame(w http.ResponseWriter, r *http.Request) error {
+	idStr := chi.URLParam(r, "id")
+	id, err := strconv.ParseInt(idStr, 10, 64)
+	if err != nil {
+		return errs.NewApiError(err, http.StatusBadRequest, "game id must be a number")
+	}
+
+	if err := h.service.DeleteGame(id); err != nil {
+		return err
+	}
+
+	w.WriteHeader(http.StatusNoContent)
+
 	return nil
 }
